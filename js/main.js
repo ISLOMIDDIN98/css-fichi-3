b07d6299d61a1fdb619ee3920037a5fe14565a18
ISLOMIDDIN98 <najmiddinovislomiddin2gmail.com>
2021-12-26 23:28:16 +0900

    add styles js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$('.card').on('click',function(){
- $('.wrapper').toggleClass('wrapper-origin');
-})
+// $('.card').on('click',function(){
+//  $('.wrapper').toggleClass('wrapper-origin');
+// });
+
+
+
+
+
+const wrapper = document.querySelector('.wrapper')
+
+wrapper.addEventListener('click',function(){
+  document.body.classList.toggle('wrapper-origin');
+});
